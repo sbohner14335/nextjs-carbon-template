b0e098acd3bdb43880682ad502cbcb24a22bbd68
Stephen Bohner <sbohner14335@gmail.com>
2025-12-12 15:35:50 -0500

    chore: update project icons and remove Vercel deployment CTA

--- app/page.tsx
+++ app/page.tsx
@@ -37,21 +37,6 @@ export default function Home() {
         </div>
         <div className={styles.ctas}>
           <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
             className={styles.secondary}
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
